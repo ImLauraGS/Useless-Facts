f7c63f662a8c2dad478f5a1f2a1f4c25f3593906
ImLauraGS <lgilsolano@gmail.com>
2023-12-14 14:50:52 +0100

    funcion showMenu mobile version

--- src/js/events.js
+++ src/js/events.js
@@ -1,7 +1,12 @@
-import { showFav } from "./btn.js";
+import { showFav, showMenu} from "./btn.js";
 
 const btnFav = document.getElementById('btn-fav');
 const menuFav =  document.getElementById('header-fav-btn');
+const btnMenu = document.getElementById("menu")
+const mobileFav =  document.getElementById('mobile-fav-btn')
+
 
 btnFav.addEventListener('click', showFav);
 menuFav.addEventListener('click', showFav);
+btnMenu.addEventListener('click', showMenu);
+mobileFav.addEventListener('click', showFav);
